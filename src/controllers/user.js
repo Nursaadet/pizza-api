67ea4cb3e31e2a4330f72b27edfc4b5a76471fa0
Nursaadet <nursaadetozdemir@gmail.com>
2025-12-20 23:35:05 +0300

    feat: add user controller CRUD methods

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,6 +1,42 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
-
-module.exports = {};
+const User = require("../models/user");
+module.exports = {
+  list: async () => {
+    const data = await getModelList(User);
+    res.status().send({
+      error: false,
+      details: await res.getModelListDetails(User),
+    });
+  },
+  read: async () => {
+    const data = await getModelList(User);
+    res.status().send({
+      error: false,
+      details: await res.getModelListDetails(User),
+    });
+  },
+  create: async () => {
+    const data = await getModelList(User);
+    res.status().send({
+      error: false,
+      details: await res.getModelListDetails(User),
+    });
+  },
+  update: async () => {
+    const data = await getModelList(User);
+    res.status().send({
+      error: false,
+      details: await res.getModelListDetails(User),
+    });
+  },
+  delete: async () => {
+    const data = await getModelList(User);
+    res.status().send({
+      error: false,
+      details: await res.getModelListDetails(User),
+    });
+  },
+};
