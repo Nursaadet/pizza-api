4d315d3549a0b6be2193eaacd7f46206846c3720
Nursaadet <nursaadetozdemir@gmail.com>
2025-12-20 23:32:44 +0300

    feat(user): add password encryption, validation and roles to User model

--- src/models/user.js
+++ src/models/user.js
@@ -4,7 +4,46 @@
 ------------------------------------------------------- */
 const { mongoose } = require("../configs/dbConnection");
 /* ------------------------------------------------------- */
+const passwordEncrypt = require("../helpers/passwordEncrypt");
+
 const UserSchema = new mongoose.Schema(
-  {},
+  { username: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+      required: true,
+      set: (password) => passwordEncrypt(password),
+      //set: passwordEncrypt
+      validate: [
+        (password) =>
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/.test(password),
+        "Password type is not correct.",
+      ],
+    },
+    email: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
+      validate: [
+        (email) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email),
+        "Please fill a valid email address",
+      ],
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },},
   { collection: "users", timestamps: true }
 );
+module.exports = mongoose.model("User", UserSchema);
