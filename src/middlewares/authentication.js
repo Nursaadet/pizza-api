b8ee4adbcb3a8ec953b9f57fff0fb49290ac4c4e
Nursaadet <nursaadetozdemir@gmail.com>
2025-12-21 23:40:14 +0300

    feat(auth): handle Bearer (JWT) and Token authentication types

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -4,17 +4,28 @@
 ------------------------------------------------------- */
 
 const Token = require("../models/token");
+const jwt = require("jsonwebtoken");
 
 module.exports = async (req, res, next) => {
+    req.user = null
   const auth = req.headers?.authorization; // Token ...tokenKey... || Bearer ...jwtAccess...
   const tokenKey = auth ? auth.split(" ") : null; // ['Token', '...tokenKey...'] || ['Bearer', '...jwtAccess...']
 
-  if (tokenKey) {
+  if (tokenKey) {  // SIMPLE TOKEN
     if (tokenKey[0] == "Token") {
       const tokenData = await Token.findOne({ token: tokenKey[1] }).populate(
         "userId"
       );
       req.user = tokenData ? tokenData.userId : false;
+       } else if (tokenKey[0] == "Bearer") { // JWT
+
+            jwt.verify(tokenKey[1], process.env.ACCESS_KEY, function (error, accessData) {
+
+                // console.log(accessData)
+                req.user = accessData ? accessData : null
+
+            })
+
     }
   }
 
